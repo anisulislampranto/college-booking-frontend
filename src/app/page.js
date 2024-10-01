a584363918b7dc73c6e6f06947235eb9ec2a97ae
Anisul Islam <anisulislam.pranto@gmail.com>
2024-10-01 12:46:32 +0600

    init front end base

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={""}>
+      <h1 className=" text-reed-400">hello</h1>
+    </main>
+  );
+}
