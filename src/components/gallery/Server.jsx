ddfccf80862d6581959dae1ea0d4a01bc0337ab9
Anisul Islam <anisulislam.pranto@gmail.com>
2024-10-05 19:26:58 +0600

    test multer image visible or not

--- src/components/gallery/Server.jsx
+++ src/components/gallery/Server.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default async function GalleryServer() {
@@ -12,8 +13,11 @@ export default async function GalleryServer() {
                 {
                     data?.map((el) => 
                         <li key={el._id} className=' relative h-56 w-full' >
-                            <img src={`${el.image}`} className=' h-56 w-full' alt={'college.image'} />
-                            {/* <Image className=' absolute object-cover rounded-md' src={`${el.image}`} alt='img' fill /> */}
+                            {
+                                console.log('el._id', el.image)
+                            }
+                            {/* <img src={`${el.image}`} className=' h-56 w-full' alt={'college.image'} /> */}
+                            <Image className=' absolute object-cover rounded-md' src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${el.image}`} alt='img' fill />
                             <p className=' absolute bottom-5 text-xl left-3 text-white capitalize backdrop-blur-md p-2'>{el.college?.name}</p>
                         </li>
                     )
