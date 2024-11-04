7b4025a8b578473a56cc6590aebdfb892261f3a3
Anisul Islam <anisulislam.pranto@gmail.com>
2024-11-04 11:42:08 +0600

    page button refactored

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import Button from '@/components/ui/button';
+import Button from '../../components/ui/Button';
 import { AuthContext } from '@/context/AuthContext';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
